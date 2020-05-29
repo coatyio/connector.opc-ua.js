9a109a2d6557ac22bf0a973154c328b6c49d1d95
ffa500 <hubertus.hohl@siemens.com>
2020-05-29 19:19:31 +0200

    fix(example): add missing communication namespace in producer agent

--- example/src/producer/agent.ts
+++ example/src/producer/agent.ts
@@ -319,6 +319,7 @@ const configuration: Configuration = {
     },
     communication: {
         brokerUrl: "mqtt://localhost:1883",
+        namespace: "coaty.opcua.example",
         shouldAutoStart: true,
     },
     controllers: {
